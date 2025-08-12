a0a758a5e079db75e2cf718d560eaa8ff70f4ced
june <lavagolem876@gmail.com>
2025-08-12 17:09:48 -0400

    beginning of the great rarity revolution 2025

---
+++ Globals/RarityTweaksGlobalitem.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace KitchenSinkRevengeance.Globals
+{
+    public class RarityTweaksGlobalItem : GlobalItem
+    {
+        public override void SetDefaults(Item entity)
+        {
+            switch (entity.type)
+            {
+                case ItemID.Shackle:
+                    entity.rare = ItemRarityID.White;
+                    break;
+                case ItemID.LavaCharm:
+                    entity.rare = ItemRarityID.Blue;
+                    break;
+                case ItemID.AnkletoftheWind:
+                    entity.rare = ItemRarityID.Blue;
+                    break;
+                case ItemID.AnkletoftheWind:
+                    entity.rare = ItemRarityID.Blue;
+                    break;
+                case ItemID.FeralClaws:
+                    entity.rare = ItemRarityID.Blue;
+                    break;
+                case ItemID.WhoopieCushion:
+                    entity.rare = ItemRarityID.Blue;
+                    break;
+                case ItemID.FeralClaws:
+                    entity.rare = ItemRarityID.Blue;
+                    break;
+                case ItemID.Harpoon:
+                    entity.rare = ItemRarityID.Blue;
+                    break;
+ 
+            }
+        }
+    }
+}
